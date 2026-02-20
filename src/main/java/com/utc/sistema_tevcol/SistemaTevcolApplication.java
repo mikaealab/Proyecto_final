9028fdd9f389084dde8d70944841a279bd47ebf3
mikaelab <nashely.betancourt6156@utc.edu.ec>
2026-02-20 11:55:03 -0500

    subida archivo raiz

---
+++ src/main/java/com/utc/sistema_tevcol/SistemaTevcolApplication.java
@@ -0,0 +1,13 @@
+package com.utc.sistema_tevcol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTevcolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTevcolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utc/sistema_tevcol/SistemaTevcolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utc.sistema_tevcol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTevcolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
