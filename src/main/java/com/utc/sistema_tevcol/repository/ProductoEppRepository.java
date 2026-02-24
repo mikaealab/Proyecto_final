af37908729f06a5909a289d042a3a9ea38efac91
mikaelab <nashely.betancourt6156@utc.edu.ec>
2026-02-23 21:43:15 -0500

    Creacion de la intercafe ProductoEpp para repository

---
+++ src/main/java/com/utc/sistema_tevcol/repository/ProductoEppRepository.java
@@ -0,0 +1,7 @@
+package com.utc.sistema_tevcol.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.utc.sistema_tevcol.entity.ProductoEpp;
+
+public interface ProductoEppRepository extends JpaRepository<ProductoEpp, Long> {
+}
